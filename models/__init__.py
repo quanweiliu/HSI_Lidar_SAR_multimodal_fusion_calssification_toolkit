6d617592586476b3c73b3eef9e355bc031b02e6e
icclab5188 <icclabjcucse@gmail.com>
2025-05-23 09:31:33 +1000

    version2.0

---
+++ models/__init__.py
@@ -0,0 +1,208 @@
+# from MS2CANet.pymodel import pyCNN
+
+# __all__ = ["pyCNN"]
+
+
+def get_model_config(args):
+
+	if args.backbone == "cnn" or args.backbone == "vit":
+		args.epochs = 100
+		args.batch_size = 64
+		args.patch_size = 13
+		args.randomCrop = 11
+		args.pca = True
+		args.components = 15
+		args.learning_rate = 0.001
+		##### original version no step_size and gamma
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.7
+
+	if args.backbone == "ViTDGCN":
+		args.data_augmentation = False
+		args.randomCrop = None
+		args.batch_size = 128
+		args.patch_size = 27
+		args.pca = True
+		args.components = 35
+		args.learning_rate = 0.0005
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.5
+
+	elif args.backbone == "FDGC":
+		args.data_augmentation = False
+		args.randomCrop = None
+		args.batch_size = 128
+		args.patch_size = 19
+		args.pca = True
+		args.components = 32
+		args.learning_rate = 0.0001
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.5
+
+	elif args.backbone == "SSFTTnet":
+		args.data_augmentation = False
+		args.randomCrop = None
+		args.batch_size = 64
+		args.patch_size = 13
+		args.pca = True
+		args.components = 30
+		args.learning_rate = 0.001
+		args.schedule = False
+		# args.step_size = 30
+		# args.gamma = 0.5
+
+
+	elif args.backbone == "morphFormer":
+		args.epochs = 500
+		args.data_augmentation = False
+		args.randomCrop = None
+		args.batch_size = 64
+		args.patch_size = 11
+		args.pca = False
+		# args.components = 30
+		args.learning_rate = 5e-4
+		args.schedule = False
+		args.FM = 16
+		args.HSIOnly = True
+		args.testSizeNumber = 100
+		# args.step_size = 30
+		# args.gamma = 0.5
+
+
+	elif args.backbone == "mamba":
+		args.epochs = 100
+		args.batch_size = 64
+		args.patch_size = 12
+		args.randomCrop = 8
+		args.pca = True
+		args.components = 15    
+		args.learning_rate = 0.001
+		##### original version no step_size and gamma
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.7
+
+
+	elif args.backbone == "MDL_M" or args.backbone == "MDL_L" or \
+		args.backbone == "MDL_E_D" or args.backbone == "MDL_C":
+		args.epochs = 150
+		args.patch_size = 7
+		args.batch_size = 64
+		args.pca = False
+		# args.components = 20
+		args.learning_rate = 0.001
+		##### original version no step_size and gamma
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.5
+
+
+	elif args.backbone == "FusAtNet":
+		args.epochs = 1000
+		args.patch_size = 11
+		args.batch_size = 64
+		args.pca = False
+		# args.components = 20
+		args.learning_rate = 0.000005
+		##### original version no step_size and gamma
+		args.schedule = False
+		# args.step_size = 30
+		# args.gamma = 0.5
+
+	elif  args.backbone == "CrossHL":
+		args.epochs = 200
+		args.patch_size = 11
+		args.batch_size = 64
+		args.pca = False
+		args.learning_rate = 0.0005
+		args.schedule = True
+		args.step_size = 50
+		args.gamma = 0.9
+
+	elif args.backbone == "MS2CANet":
+		args.epochs = 100
+		args.patch_size = 11
+		args.batch_size = 64
+		args.learning_rate = 0.001
+		args.pca = True
+		args.components = 20    # Houston
+		##### original version no step_size and gamma
+		args.schedule = False
+		# args.step_size = 30
+		# args.gamma = 0.5
+
+	elif args.backbone == "S2ENet":
+		args.epochs = 128
+		args.batch_size = 64
+		args.patch_size = 7
+		args.pca = True
+		args.components = 20    # Houston
+		args.learning_rate = 0.001
+		args.schedule = True
+
+	elif args.backbone == "HCTNet":
+		args.epochs = 100
+		args.batch_size = 64
+		args.patch_size = 11
+		args.pca = True
+		args.components = 20    # Houston
+		# args.components = 30    # Trento_train2
+		args.learning_rate = 0.001
+		##### original version no step_size and gamma
+		args.schedule = False
+		# args.step_size = 30
+		# args.gamma = 0.5
+
+	elif args.backbone == "SHNet":
+		args.epochs = 100
+		args.batch_size = 128
+		args.patch_size = 7
+		args.pca = False
+		args.factors = 4
+		# args.factors = 8
+		args.learning_rate = 0.001
+		args.weight_decay = 0.001
+		args.lb_smooth = 0.01
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.5
+
+	elif args.backbone == "DSHFNet":
+		args.epochs = 500
+		args.batch_size = 64
+		args.patch_size = 6
+		args.pca = False
+		args.learning_rate = 5e-4
+		args.weight_decay = 0
+		args.schedule = True
+		args.step_size = args.epochs // 10
+		args.gamma = 0.9
+
+	elif args.backbone == "MIViT":
+		args.epochs = 500
+		args.patch_size = 8
+		args.batch_size = 64
+		args.pca = False
+		args.learning_rate = 1e-4
+		args.weight_decay = 0.001
+		args.schedule = True
+		args.step_size = args.epochs // 10
+		args.gamma = 0.9
+		args.fusion = 'TTOA'
+		args.pred_flag = 'o_fuse'
+		
+	else:
+		args.epochs = 100
+		args.batch_size = 64
+		args.patch_size = 11
+		# args.patch_size = 12
+		# args.randomCrop = 8
+		args.learning_rate = 0.001
+		args.pca = True
+		args.components = 15    
+		args.schedule = True
+		args.step_size = 30
+		args.gamma = 0.5
---
+++ readme.md
@@ -0,0 +1,53 @@
+这个版本改成 Linux 版本，需要更改的内容：
+
+- 导入包
+!pip install spectral
+from google.colab import drive
+drive.mount('/content/drive')
+
+- 添加环境变量
+sys.path.append('/content/drive/MyDrive/code/MDL/')
+
+
+
+
+Code and Paper source: 
+<!-- - [EndNet](https://github.com/danfenghong/IEEE_GRSL_EndNet)
+- [MDL](https://github.com/danfenghong/IEEE_TGRS_MDL-RS)
+- [HCTNet](https://github.com/zgr6010/Fusion_HCT) --------------------------------- [Paper](https://ieeexplore.ieee.org/document/9999457)
+- [FusAtNet](https://github.com/ShivamP1993/FusAtNet-Dual-Attention-based-SpectroSpatial-Multimodal-Fusion-Network-for-Hyperspectral-and-LiDAR-)
+- [S2ENet](https://github.com/likyoo/Multimodal-Remote-Sensing-Toolkit)
+- [Cross-HL](https://github.com/AtriSukul1508/Cross-HL)-------------------------------- [Paper](https://ieeexplore-ieee-org.elibrary.jcu.edu.au/document/10462184)
+- [MIViT](https://github.com/icey-zhang/MIViT)
+- [MS2CANet](https://github.com/junhengzhu/MS2CANet)
+- [SHNet](https://github.com/quanweiliu/SHNet)
+--> 
+
+
+Code and Paper source: 
+| Code      | Paper |  Journal |  Year | 
+| ----------- | ----------- |----------- |----------- |
+| [EndNet](https://github.com/danfenghong/IEEE_GRSL_EndNet)      | Title       | Title       | Title       |
+| [MDL](https://github.com/danfenghong/IEEE_TGRS_MDL-RS)   | Text        | Title       |
+| [HCTNet](https://github.com/zgr6010/Fusion_HCT)   | [Paper](https://ieeexplore.ieee.org/document/9999457)        | TGRS       | 2022       |
+| [FusAtNet](https://github.com/ShivamP1993/FusAtNet-Dual-Attention-based-SpectroSpatial-Multimodal-Fusion-Network-for-Hyperspectral-and-LiDAR-)   | Text        | CVPR        | 2020       |
+| [S2ENet](https://github.com/likyoo/Multimodal-Remote-Sensing-Toolkit)   | Text        | Title       | Title       |
+| [Cross-HL](https://github.com/AtriSukul1508/Cross-HL)  | [Paper](https://ieeexplore-ieee-org.elibrary.jcu.edu.au/document/10462184)        | Title       | Title       |
+| [MIViT](https://github.com/icey-zhang/MIViT)   | Text        | Title       | Title       |
+| [MS2CANet](https://github.com/junhengzhu/MS2CANet)   | Text        | Title       | Title       |
+| [SHNet](https://github.com/quanweiliu/SHNet)   | Text        | Title       | Title       |
+
+
+This repository proposed a new taxonomy to descibe existed patch based image classification (semantic segmenation) models.
+
+Based the input and output of the model, we categorized these pixel-level classification model into singlesacle singlemodality input and singleoutput (SSISO), singlesacle multimodelity input and singleoutput (SMISO), singlesacle multimodelity input and multioutput (SMIMO), mutlisacle multimodelity input and singleoutput (MMISO), mutlisacle multimodelity input and multiouput (MMIMO).
+
+Of course, there are multiscale singlemodality input, singleoutput (MSISO) and multiscale singlemodality input and multioutput (MSIMO) and so on. We will continue and add them in this framework.
+
+I have collected a range of models based this taxonomy. If you want to contribute this repository and make it better, feel free to contact me. My emial : quanwei.liu@my.jcu.edu.au
+
+
+
+Noting: 
+- MS2CANet : 原始的代码
+- MS2CANet2 : 将代码拆分了两部分，测试精度，结果是一模一样的
